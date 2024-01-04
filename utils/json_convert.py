dd07ee6baa207854ca529c0cdd34e90c23167508
Soeun Uhm <uhmturks@yonsei.ac.kr>
2024-01-05 00:34:34 +0900

    chore : move directory to utils

--- utils/json_convert.py
+++ utils/json_convert.py
@@ -3,12 +3,12 @@ import jsonlines, json
 
 results = []
 
-with open('output/temp_output.jsonl', 'r') as file:
+with open('../output/temp_output.jsonl', 'r') as file:
     for line in file:
         json_data = json.loads(line)
         results.append(json_data)
 
 json_output = json.dumps(results, indent=4)
 
-with open('output/final_output.json', 'w') as output_file:
+with open('../output/final_output.json', 'w') as output_file:
     output_file.write(json_output)
