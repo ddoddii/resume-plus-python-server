e9a5edac1647edb68b11d770611be95ed6e0c1e7
Soeun Uhm <uhmturks@yonsei.ac.kr>
2024-01-05 19:04:31 +0900

    :white_check_mark: sample test code

---
+++ test/test.py
@@ -0,0 +1,12 @@
+from fastapi.testclient import TestClient
+
+from main import app
+
+client = TestClient(app)
+
+
+def test_read_main():
+    response = client.get("/test")
+    assert response.status_code == 200
+    assert response.json() == {"message": "test"}
+
