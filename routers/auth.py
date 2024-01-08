88c959569044e54c009c1f6081bcd03a763b89e7
Soeun Uhm <uhmturks@yonsei.ac.kr>
2024-01-08 15:58:35 +0900

    :ambulance: hotfix : update api /auth/login to use BaseModel

--- routers/auth.py
+++ routers/auth.py
@@ -51,7 +51,7 @@ async def create_user(db: db_dependency, create_user_request: CreateUserRequest)
 
 @router.post("/login", response_model=Token)
 async def login_for_access_token(
-    form_data: Annotated[OAuth2PasswordRequestForm, Depends()], db: db_dependency
+    form_data: UserLoginRequest, db: db_dependency
 ) -> dict:
     user = authenticate_user(form_data.username, form_data.password, db)
     if not user:
