6313fd5676e1c53ca158bf0ec8bfb3e147632573
Soeun Uhm <uhmturks@yonsei.ac.kr>
2024-03-23 18:07:23 +0900

    chore : 주석 삭제

--- src/models.py
+++ src/models.py
@@ -1,8 +1,6 @@
 from sqlalchemy import Column, Integer, String, String, Text, ForeignKey
 from sqlalchemy.ext.declarative import declarative_base
 
-# from database.setup import Base
-
 Base = declarative_base()
 
 
