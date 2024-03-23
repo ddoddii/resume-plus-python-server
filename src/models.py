63e7fcf46e8fa5d3a8a231ac46342d7ec977190f
Soeun Uhm <uhmturks@yonsei.ac.kr>
2024-03-24 00:20:31 +0900

    refactor : 스키마 분리

--- src/models.py
+++ src/models.py
@@ -4,55 +4,6 @@ from sqlalchemy.ext.declarative import declarative_base
 Base = declarative_base()
 
 
-class Users(Base):
-    __tablename__ = "users"
-
-    id = Column(Integer, nullable=False, primary_key=True, index=True)
-    username = Column(String(20), nullable=False, index=True, unique=True)
-    name = Column(String(20), nullable=False)
-    hashed_password = Column(String(64), nullable=False)
-    email = Column(String(30))
-    session_count = Column(Integer, default=0)
-
-
-class CV(Base):
-    __tablename__ = "cv"
-
-    id = Column(Integer, nullable=False, primary_key=True, index=True)
-    user_id = Column(Integer, ForeignKey("users.id"))
-    content = Column(Text, nullable=False)
-    position = Column(String(20), nullable=True)
-    upload_time = Column(Text, nullable=False)
-
-
-class Tech_Question(Base):
-    __tablename__ = "tech_question"
-
-    id = Column(Integer, nullable=False, autoincrement=True, primary_key=True)
-    question = Column(Text, nullable=False)
-    position = Column(String(20))
-    topic = Column(String(20))
-    example_answer = Column(Text)
-
-
-class Behavior_Question(Base):
-    __tablename__ = "behavior_question"
-
-    id = Column(Integer, nullable=False, autoincrement=True, primary_key=True)
-    question = Column(Text, nullable=False)
-    criteria = Column(String(20))
-
-
-class Personal_Question(Base):
-    __tablename__ = "personal_question"
-
-    id = Column(Integer, nullable=False, autoincrement=True, primary_key=True)
-    cv_id = Column(Integer, ForeignKey("cv.id"))
-    session_id = Column(Integer, ForeignKey("session.id"))
-    question = Column(Text, nullable=False)
-    criteria = Column(Text)
-
-
 class Sessions(Base):
     __tablename__ = "session"
 
